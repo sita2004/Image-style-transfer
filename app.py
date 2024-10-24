b2fb6c604bd884e267f84e08a738c0e505d34b8e
sita2004 <151060405+sita2004@users.noreply.github.com>
2024-10-24 17:33:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def stylize():
     return jsonify({'stylized_image': img_base64})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
